5023947c984f13b800581acd377ed39534a1aa3a
Ecky819 <marcoeggert73@gmail.com>
2025-06-19 10:44:06 +0200

    Funktion erstellt

--- Task sheet/main.dart
+++ Task sheet/main.dart
@@ -1,3 +1,9 @@
+void printNumber(int number) {
+  print("Die Zahl ist: $number");
+}
+
 void main() {
-  // Create a new instance of the Counter class
+  printNumber(5);
+  printNumber(10);
+  printNumber(15);
 }
